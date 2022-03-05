cc2476bb71d4d1ea336b369e90905bc6356c5ba1
Harshit Yadav <harshity8@Gmail.com>
2022-03-05 19:58:18 +0530

    clean coded

--- test/test.js
+++ test/test.js
@@ -1,28 +1,14 @@
 const { ethers } = require("hardhat");
-//const Web3 = require("web3");
+const {hexlify} =  require('ethers-utils');
 const EthUtil = require('ethereumjs-util')
 const { soliditySha3 } = require("web3-utils");
 const dotenv = require('dotenv');
-const {
-  BigNumber,
-  bigNumberify,
-  getAddress,
-  hexlify,
-  keccak256,
-  defaultAbiCoder,
-  toUtf8Bytes,
-  solidityPack
-} =  require('ethers-utils');
 dotenv.config();
-
 
 const alchemy_key = process.env.ALCHEMY_API_KEY;
 const private_key = process.env.PRIVATE_KEY;
-//const ALCHEMY_POINT = `https://eth-mainnet.alchemyapi.io/v2/${alchemy_key}`;
-//const web3 = new Web3(new Web3.providers.HttpProvider(ALCHEMY_POINT));
 
 describe("starting tests", function () {
-    const usdc_on_aave="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
     let sender;
     let spender;
     const aave_token_address="0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9";
@@ -30,48 +16,40 @@ describe("starting tests", function () {
     const address_having_no_aave_tokens = "0x3Fc046bdE274Fe8Ed2a7Fd008cD9DEB2540dfE36";
     let contract;
 
-    let owner;
-    let acc1;
-    let acc2;
-    let contract1;
-    let instance;
-
   
     before(async () =>{
 
-      [owner, acc1 ,acc2] = await ethers.getSigners();
-      contract1 = await ethers.getContractFactory("main");
-      instance = await contract1.deploy();
-      await instance.deployed();
-
+      // impersonating a acccount that has some AAVE tokens
       await hre.network.provider.request({
         method: "hardhat_impersonateAccount",
         params: [address_having_aave_tokens],
       });
-
       sender=await ethers.getSigner(address_having_aave_tokens);
 
+      //impersonating my account, i have the private key of it
       await hre.network.provider.request({
         method: "hardhat_impersonateAccount",
         params: [address_having_no_aave_tokens],
       });
+      spender=await ethers.getSigner(address_having_no_aave_tokens);
 
+      //giving myself some transaction fee
       await network.provider.send("hardhat_setBalance", [
         address_having_no_aave_tokens,
         "0x1000000000000000000000000000000000",
-      ]);    // will need to remove this
+      ]);    // will need to remove this, this is so that i can make transactions locally as i have no eth
 
-      spender=await ethers.getSigner(address_having_no_aave_tokens);
 
-      usdc_token = await ethers.getContractAt("IERC20_functions",usdc_on_aave);
-
+      //creating instance of the AAVE token contract
       aave_token_contract = await ethers.getContractAt("aave_token_functions", aave_token_address);
       
+
+      // needed for getting public variables necessary for hashing
       const abi_contract = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"delegator","type":"address"},{"indexed":true,"internalType":"address","name":"delegatee","type":"address"},{"indexed":false,"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"DelegateChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"DelegatedPowerChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"DELEGATE_BY_TYPE_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"DELEGATE_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"EIP712_REVISION","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"REVISION","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"_aaveGovernance","outputs":[{"internalType":"contract ITransferHook","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"_nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"_votingSnapshots","outputs":[{"internalType":"uint128","name":"blockNumber","type":"uint128"},{"internalType":"uint128","name":"value","type":"uint128"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"_votingSnapshotsCounts","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegatee","type":"address"}],"name":"delegate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegatee","type":"address"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"uint256","name":"expiry","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"delegateBySig","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegatee","type":"address"},{"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"delegateByType","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegatee","type":"address"},{"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"uint256","name":"expiry","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"delegateByTypeBySig","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegator","type":"address"},{"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"getDelegateeByType","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"blockNumber","type":"uint256"},{"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"getPowerAtBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"},{"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"getPowerCurrent","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"blockNumber","type":"uint256"}],"name":"totalSupplyAt","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}];
-      
       contract = new web3.eth.Contract(abi_contract, "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9")
   
     });
+
     
     describe("tests", function(){
         it("testing_balance", async function(){
@@ -84,10 +62,10 @@ describe("starting tests", function () {
             console.log("balance in sender's account after transaction: ",await aave_token_contract.balanceOf(sender.address));
             console.log("balance in spender's account after transaction:",await aave_token_contract.balanceOf(spender.address));
 
-            //spender and sender has been interhcanged as i have only my private key so i can sign with it only
+            //spender and sender have been interhcanged as i have only my private key so i can sign with it only
         });
         
-        it("testing permit", async function(){
+        it("creating hash, signing, calling permit, transfer", async function(){
 
             const deadline_key=1000000000000 //to be safe
 
@@ -116,10 +94,12 @@ describe("starting tests", function () {
 
             //calling the transfer from from sender to transfer the amount that was allowed by the spender (Remember that the sender and spender were interhcnaged)
             await aave_token_contract.connect(sender).transferFrom(spender.address,sender.address,"10000000");
+            console.log("The transaction has been made using permit and transfer functions")
+
 
             //getting the finla balances
-            console.log("balance in sender's account: ",await aave_token_contract.balanceOf(sender.address));
-            console.log("balance in spender's account :",await aave_token_contract.balanceOf(spender.address));
+            console.log("balance in sender's account now: ",await aave_token_contract.balanceOf(sender.address));
+            console.log("balance in spender's account now: ",await aave_token_contract.balanceOf(spender.address));
         });
 
 
