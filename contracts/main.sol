daf013241ca376ee0303f79b126287863a98b383
Harshit Yadav <harshity8@Gmail.com>
2022-03-05 17:11:00 +0530

    finally_worked

--- contracts/main.sol
+++ contracts/main.sol
@@ -7,7 +7,6 @@ import "./interface.sol";
 
 import "hardhat/console.sol";
 
-import "./recover.sol";
 
 
 contract main{
@@ -16,6 +15,38 @@ contract main{
         //console.log(inert);
         return inert;
     }*/
+
+    function splitSignature(bytes memory sig)
+        public
+        pure
+        returns (
+            bytes32 r,
+            bytes32 s,
+            uint8 v
+        )
+    {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            /*
+            First 32 bytes stores the length of the signature
+
+            add(sig, 32) = pointer of sig + 32
+            effectively, skips first 32 bytes of signature
+
+            mload(p) loads next 32 bytes starting at the memory address p into memory
+            */
+
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+        return (r, s, v);
+    }
+
     function func(bytes32 PERMIT_TYPEHASH, address owner,address spender,uint256 value,uint256 currentValidNonce,uint256 deadline) external pure returns (bytes32) {
         return keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, currentValidNonce, deadline));
     }
@@ -42,6 +73,7 @@ contract main{
     require(owner != address(0), 'INVALID_OWNER');
     //solium-disable-next-line
     //require(block.timestamp <= deadline, 'INVALID_EXPIRATION');
+    //uint256 currentValidNonce = _nonces[owner];
     bytes32 digest =
       keccak256(
         abi.encodePacked(
@@ -50,13 +82,9 @@ contract main{
           keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, currentValidNonce, deadline))
         )
       );
-      //console.log("nln",v);
-      //console.log(r);
-      //console.log(s);
-    address x = ecrecover(digest, v, r, s);
-    //console.log(x);
-    //require(owner == ecrecover(digest, v, r, s), 'INVALID_SIGNATURE');
-    return x;
-
+    return ecrecover(digest, v, r, s);
+    require(owner == ecrecover(digest, v, r, s), 'INVALID_SIGNATURE');
+    //_nonces[owner] = currentValidNonce.add(1);
+    //_approve(owner, spender, value);
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,7 +1,18 @@
 const { ethers } = require("hardhat");
 //const Web3 = require("web3");
+const EthUtil = require('ethereumjs-util')
 const { soliditySha3 } = require("web3-utils");
 const dotenv = require('dotenv');
+const {
+  BigNumber,
+  bigNumberify,
+  getAddress,
+  hexlify,
+  keccak256,
+  defaultAbiCoder,
+  toUtf8Bytes,
+  solidityPack
+} =  require('ethers-utils');
 dotenv.config();
 
 
@@ -58,7 +69,7 @@ describe("starting tests", function () {
       
       const abi_contract = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"delegator","type":"address"},{"indexed":true,"internalType":"address","name":"delegatee","type":"address"},{"indexed":false,"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"DelegateChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"DelegatedPowerChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"DELEGATE_BY_TYPE_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"DELEGATE_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"EIP712_REVISION","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"REVISION","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"_aaveGovernance","outputs":[{"internalType":"contract ITransferHook","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"_nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"_votingSnapshots","outputs":[{"internalType":"uint128","name":"blockNumber","type":"uint128"},{"internalType":"uint128","name":"value","type":"uint128"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"_votingSnapshotsCounts","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegatee","type":"address"}],"name":"delegate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegatee","type":"address"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"uint256","name":"expiry","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"delegateBySig","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegatee","type":"address"},{"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"delegateByType","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegatee","type":"address"},{"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"uint256","name":"expiry","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"delegateByTypeBySig","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegator","type":"address"},{"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"getDelegateeByType","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"blockNumber","type":"uint256"},{"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"getPowerAtBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"},{"internalType":"enum IGovernancePowerDelegationToken.DelegationType","name":"delegationType","type":"uint8"}],"name":"getPowerCurrent","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"blockNumber","type":"uint256"}],"name":"totalSupplyAt","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}];
       
-      contract = new web3.eth.Contract(abi_contract, "0xC13eac3B4F9EED480045113B7af00F7B5655Ece8")
+      contract = new web3.eth.Contract(abi_contract, "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9")
   
     });
     
@@ -80,12 +91,6 @@ describe("starting tests", function () {
 
             //spender and sender has been interhcanged as i have only my private key
 
-
-            const a = await web3.eth.accounts.sign('Some data', private_key);
-            console.log(a);
-            const y = await web3.eth.accounts.recover('Some data', a.signature)
-            console.log(y);
-            console.log("END");
             let PERMIT_TYPEHASH = await contract.methods.PERMIT_TYPEHASH().call();
             let currentValidNonce = await contract.methods._nonces(spender.address).call();
             let DOMAIN_SEPARATOR = await contract.methods.DOMAIN_SEPARATOR().call();
@@ -113,26 +118,19 @@ describe("starting tests", function () {
             const hash1_for_encodePacked = soliditySha3('\x19\x01', DOMAIN_SEPARATOR, hash);
             console.log("final here", hash1_for_encodePacked);
 
-            var c = await web3.eth.accounts.sign(hash1_for_encodePacked, private_key);
-            //console.log(c);
-            const {signature, messageHash, v, r, s} = c;
-
-            const d = await web3.eth.accounts.recover({messageHash: messageHash, v: v, r: r, s: s});
-            console.log(d);
-
-            console.log("--------");
-            console.log(v);
-            console.log(r);
-            console.log(s);
-            console.log("---------");
-
+            const { v, r, s } = EthUtil.ecsign(Buffer.from(hash1_for_encodePacked.slice(2), 'hex'), Buffer.from(private_key.slice(2), 'hex'));
 
             //spender and sender has been interchanged, sender is me.
-            const check=await instance.permit(PERMIT_TYPEHASH,DOMAIN_SEPARATOR,currentValidNonce,spender.address,sender.address,10000000,1000000000000,c.v,c.r,c.s);
+            const check=await instance.permit(PERMIT_TYPEHASH,DOMAIN_SEPARATOR,currentValidNonce,spender.address,sender.address,10000000,1000000000000,v,hexlify(r), hexlify(s));
             console.log(check);
 
             console.log("should be:", spender.address);
-            //await aave_token_contract.connect(sender).permit(spender.address,sender.address,10000000,1000000000000,c.v,c.s,c.r);
+            await aave_token_contract.permit(spender.address,sender.address,10000000,1000000000000,v, hexlify(r), hexlify(s));
+            console.log("permited by me to the sender to transfer");
+            console.log(await contract.methods.balanceOf(sender.address).call())
+            await aave_token_contract.connect(sender).transferFrom(spender.address,sender.address,"10000000");
+            console.log("balance in sender's account: ",await aave_token_contract.balanceOf(sender.address));
+            console.log("balance in spender's account :",await aave_token_contract.balanceOf(spender.address));
         });
 
 
